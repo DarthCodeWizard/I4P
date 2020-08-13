72e1f2622e17db661137754b139fc8ccc5b27098
DarthCodeWizard <mate.zsozso@gmail.com>
2020-08-13 22:30:52 +0200

    This is the first day of the escape-room game project.

---
+++ I4PEscpaeGame/I4PEscpaeGame/places/BathRoom/Things.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4PEscpaeGame.places.BathRoom
+{
+    class Things
+    {
+        public bool IsMooveable { get; set; }
+
+        public List <string> Functions { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
